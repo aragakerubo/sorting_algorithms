709d2c0fd8f3bec999d677690858e209d3a59052
aragakerubo <aragakerubo@gmail.com>
2024-02-18 00:06:16 -0500

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,8 +12,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	/* if (array == NULL || size < 2) */
-	/* 	return; */
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
