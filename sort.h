4df8c7572d793512fdbcf33d0e5f492cbba4d84d
aragakerubo <aragakerubo@gmail.com>
2024-02-23 21:24:42 -0500

    10. Bitonic sort

--- sort.h
+++ sort.h
@@ -42,5 +42,9 @@ void merge(size_t start, size_t middle, size_t end, int *dest, int *source);
 void heap_sort(int *array, size_t size);
 void heapify(int *array, size_t size, size_t i, size_t size_orig);
 void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
+void swap_integers(int *a, int *b);
 
 #endif /* SORT_H */
